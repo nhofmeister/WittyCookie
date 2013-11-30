5b989152661c3114637190244109587d6c0900d8
Holger Tewis <h.tewis@bigpoint.net>
2013-11-30 14:52:12 +0100

    more texts

--- wittycookie/wittycookie.js
+++ wittycookie/wittycookie.js
@@ -5,7 +5,11 @@ var getRandomMessage = function()
                                "Do or do not, there is no try. - Master Yoda",
                                "Du hast es nicht vergessen, du hast es nicht gemacht! - ein Mathelehrer",
                                "It is possible to commit no errors and still lose. That is not a weakness, that is live. - Captain Picard to Data",
-                               "I could have killed 'em all, I could've killed you. In town you're the law, out here it's me. Don't push it! Don't push it or I'll give you a war you won't believe. Let it go. Let it go! - John Rambo");   
+                               "I could have killed 'em all, I could've killed you. In town you're the law, out here it's me. Don't push it! Don't push it or I'll give you a war you won't believe. Let it go. Let it go! - John Rambo",
+                               "Handle nur nach derjenigen Maxime, durch die du zugleich wollen kannst, dass sie ein allgemeines Gesetz werde.",
+                               "Ein Hamsterrad sieht von innen aus wie eine Leiter.",
+                               "In jedem Scheitern steckt eine Chance.",
+                               "Irren ist menschlich.");   
    var i = Math.floor(Math.random()*wittyMessages.length);
    console.log(i);
    return wittyMessages[i];
