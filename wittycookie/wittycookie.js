089eb4397c2198446fc1d0a4fffb50711f70f703
Alexander Stolle <a.stolle@bigpoint.net>
2013-11-30 14:43:21 +0100

    neue texte hinzu gefügt

--- wittycookie/wittycookie.js
+++ wittycookie/wittycookie.js
@@ -1,6 +1,11 @@
 var getRandomMessage = function()
 {
-   var wittyMessages=new Array("foo","bar","blub");
+   var wittyMessages=new Array("Rotation. Ich beruehre die Tastatur. Sonnenaufgang.",
+                               "Von mir lernen, heisst siegen lernen. - Holg0r",
+                               "Do or do not, there is no try. - Master Yoda",
+                               "Du hast es nicht vergessen, du hast es nicht gemacht! - ein Mathelehrer",
+                               "It is possible to commit no errors and still lose. That is not a weakness, that is live. - Captain Picard to Data",
+                               "I could have killed 'em all, I could've killed you. In town you're the law, out here it's me. Don't push it! Don't push it or I'll give you a war you won't believe. Let it go. Let it go! - John Rambo");   
    var i = Math.floor(Math.random()*wittyMessages.length);
    console.log(i);
    return wittyMessages[i];
