1e84cfe03eeda1231558bfe14ac4720e6a5c4f31
sirrgang <steffen.irrgang@googlemail.com>
2013-11-30 12:27:59 +0100

    fixed merged error

--- wittycookie/wittycookie.js
+++ wittycookie/wittycookie.js
@@ -4,12 +4,6 @@ if (Meteor.isClient) {
   };
 
   Template.hello.events({
-
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-
     'click #b1' : function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
