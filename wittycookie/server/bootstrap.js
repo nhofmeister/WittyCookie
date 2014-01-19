97c4207bff4a501ede1e9ab0ea0f8065897ddb5a
Holger Tewis <htewis@holgers-mac-pro.fritz.box>
2014-01-19 22:16:10 +0100

    all proverbs reactivated

--- wittycookie/server/bootstrap.js
+++ wittycookie/server/bootstrap.js
@@ -17,20 +17,17 @@ Meteor.startup(function () {
           Lists.insert({ "proverb" : "Von mir lernen, heißt siegen lernen. - Holg0r",
           "tags" : [ "german","citation"]}); 
 
-    /*var data = [
-      {name: "proverbs",
-       contents: [
-         ["Rotation. Ich beruehre die Tastatur. Sonnenaufgang.", "german", "haiku"],
-         ["Ein Hamsterrad sieht von innen aus wie eine Leiter.","german"],
-         ["In jedem Scheitern steckt eine Chance.","german", "wisdom"],
-         ["Irren ist menschlich.", "german", "wisdom"],
-         ["Von mir lernen, heisst siegen lernen. - Holg0r", "german", "citation"],
-         ["Do or do not, there is no try. - Master Yoda", "english", "star wars"],
-         ["Du hast es nicht vergessen, du hast es nicht gemacht! - ein Mathelehrer", "german", "citation"],
-         ["It is possible to commit no errors and still lose. That is not a weakness, that is live. - Captain Picard to Data", "english", "star trek"],
-         ["I could have killed 'em all, I could've killed you. In town you're the law, out here it's me. Don't push it! Don't push it or I'll give you a war you won't believe. Let it go. Let it go! - John Rambo", "english", "rambo"],
-         ["Handle nur nach derjenigen Maxime, durch die du zugleich wollen kannst, dass sie ein allgemeines Gesetz werde.", "english", "wisdom"]
-       ]*/ 
+          Lists.insert({ "proverb" : "Irren ist menschlich.",
+          "tags" : [ "german","wisdom"]}); 
+          Lists.insert({ "proverb" : "Du hast es nicht vergessen, du hast es nicht gemacht! - ein Mathelehrer",
+          "tags" : [ "german","citation"]}); 
+          Lists.insert({ "proverb" : "It is possible to commit no errors and still lose. That is not a weakness, that is live. - Captain Picard to Data",
+          "tags" : [ "english","star trek"]}); 
+          Lists.insert({ "proverb" : "I could have killed 'em all, I could've killed you. In town you're the law, out here it's me. Don't push it! Don't push it or I'll give you a war you won't believe. Let it go. Let it go! - John Rambo",
+          "tags" : [ "english","rambo"]}); 
+          Lists.insert({ "proverb" : "Handle nur nach derjenigen Maxime, durch die du zugleich wollen kannst, dass sie ein allgemeines Gesetz werde.",
+          "tags" : [ "english","wisdom"]}); 
+
       };
     
 
